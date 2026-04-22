9b30ca3e1eb3e00a0309dbf7a2133cf352b40253
L <steven10532@gmail.com>
2026-04-22 20:13:58 +0800

    append Test VTuberCard TalentCard

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== VTuber Card Battle");
-        System.out.println("Game Loading .....");
+        System.out.println("=== VTuber Card Battle ===");
+
+        // 測試 VTuber 卡
+        VTuberCard earendel = new VTuberCard("厄倫蒂兒", 5, "DEEP_SPACE", 4, 6);
+        System.out.println(earendel.display());
+
+        // 測試才藝卡
+        TalentCard heal = new TalentCard("歌回直播", 3, "DEEP_SPACE", "HEAL", 5);
+        System.out.println(heal.display());
     }
 }
